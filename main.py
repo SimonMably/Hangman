28d151fd73a1dbe7e5463c0a189a272a6610af39
SimonMably <simonmably@gmail.com>
2023-05-10 20:26:03 +0100

    Completed Hangman game. Added screenshot of game as a jpg image.

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import os
-import random
 import requests
 import dotenv
 import hangman_stages
 
 def main():
+    """Game loop."""
 
     attempts = 0
 
     correct_word = get_word()
-    print(correct_word)
     
     letters_guessed = []
-    #secret_word = "".join("_" for letter in correct_word if letter not in letters_guessed)
+    
     secret_word = ["_"] * len(correct_word)
 
 
-    while attempts < 7:
-        
-        print(attempts)
+    while attempts != 6:
+        if attempts == 0 and len(letters_guessed) == 0:
+            print("".join(secret_word))
 
-        guess = input("Guess a letter: ")
+        print(determine_stage(attempts))
+        # print(attempts)
+
+        guess = input("\nGuess a letter: ")
 
         for i, letter in enumerate(correct_word):
             # Check if current looped letter is equal to the guess
@@ -28,53 +30,57 @@ def main():
                 # Set the underscore at the position to the correct letter
                 secret_word[i] = letter
         
-        print(f"Secret word: {''.join(secret_word)}")
-
-    
-
-        #print(secret_word)
+        print(f"\n{''.join(secret_word)}\n")
 
         # if secret word has been correctly guessed
         if "_" not in secret_word:
-            print("You won")
+            print("\nCongratulations!! You won!")
             break
 
         if len(guess) > 1 or guess in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
-            print("Please only input a single letter / No numbers.")
+            print("Invalid guess. Please only input a single letter / No numbers.")
+            continue
+
+        if guess == "" or guess == " ":
+            print("Invalid guess. Guesses must contain a letter.")
             continue
 
         if guess in letters_guessed:
             print("You have already guessed that letter. Try again.")
+            continue
         else:
             letters_guessed.append(guess)
 
         if guess in correct_word:
-            print("That letter is correct")
+            print("\nThat letter is correct")
         else:
             attempts += 1
-            print("Incorrect")
+            print("\nIncorrect")
         
-        # TODO: If player lost game
-
-
+        if attempts == 6:
+            print(determine_stage(attempts))
+            print(f"\nYou lost!! The correct word is {correct_word}")
 
     keep_playing = True
     while keep_playing == True:
         
-        ask_for_new_game = input("New game (y/n)? ").lower()
+        ask_for_new_game = input("\nNew game (y/n)? ").lower()
         if ask_for_new_game == "n":
-            print("Thanks for playing :)")
+            print("\nThanks for playing :)")
             keep_playing = False
             break
         elif ask_for_new_game == "y":
+            clear_screen()
             main()
         else:
-            print("Please type either y or n")
+            print("\nPlease type either y or n")
             continue
 
-
-
 def get_word():
+    """Retrieves a random word via Free Random Word Generator API hosted on RapidAPI. Returns response.json().
+    
+    API url: https://free-random-word-generator-api.p.rapidapi.com/random-word
+    """
     dotenv.load_dotenv()
 
     API_KEY = os.getenv("API_KEY")
@@ -91,10 +97,31 @@ def get_word():
 
     return response.json()
 
-def determine_stage():
-    """"""
-    wrong_guess_amount = 0
+def determine_stage(attempt: int):
+    """Determine which hangman image to show, depending on incorrect attempts to guess the word. Returns stage depending on failed attempt amount."""
+    if attempt == 0:
+        return hangman_stages.empty_gallows
+    elif attempt == 1:
+        return hangman_stages.add_head
+    elif attempt == 2:
+        return hangman_stages.add_torso
+    elif attempt == 3:
+        return hangman_stages.add_left_arm
+    elif attempt == 4:
+        return hangman_stages.add_right_arm
+    elif attempt == 5:
+        return hangman_stages.add_right_leg
+    elif attempt == 6:
+        return hangman_stages.add_left_leg
 
+def clear_screen():
+    """Clears terminal screen. Works for Window (tested and works). Also account for Linux and Mac OS, but not tested"""
+    # For windows
+    if os.name == "nt":
+        _ = os.system("cls")
+    else:
+        # For Linux / Mac OS
+        _ = os.system("clear")
 
 
 if __name__ == "__main__":
